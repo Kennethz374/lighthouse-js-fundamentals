7e9b3a410de95ae3a0d7b03b361e96326f3b7b2e
sally <kennethzhang374@gmail.com>
2019-06-30 17:05:04 +0000

    Add more condition examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,23 @@ if (cold){
 }
 
 console.log("Now you're ready to go outside!");
+
+//logical operator
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18){
+  console.log("You are elighible to vote.");
+}
+
+//example of OR
+const temp = 80;
+
+if (temp < -40 || temp > 40){
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+//Not operator
+if (!raining){
+  console.log("Leave your umbrella at home!");
+}
